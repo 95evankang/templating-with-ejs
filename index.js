bde7d5f3109e6c10a6682d0055a2e0391b7db70e
Evan J. Kang <95evankang@gmail.com>
2022-03-09 17:42:15 +0000

    adding 3 folders users,partials pages

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
+const port = 3000;
 
-app.set('view engine','ejs';)
+app.set('view engine','ejs');
+
 
 app.get('/', (req, res) => {
   var title = 'Our Home Page';
   res.render('index',{title,title});
-})
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
