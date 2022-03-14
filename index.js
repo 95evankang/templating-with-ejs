b6da0216da6df7ead05daca9c2fca5eaba89df99
Evan J. Kang <95evankang@gmail.com>
2022-03-14 08:28:21 +0000

    Fin

--- index.js
+++ index.js
@@ -4,10 +4,34 @@ const port = 3000;
 
 app.set('view engine','ejs');
 
+//this will allow us to serve up static files, CSS, images & JS
+app.use(express.static(__dirname));
+
 
 app.get('/', (req, res) => {
   var title = 'Our Home Page';
-  res.render('index',{title,title});
+  res.render('pages/index',{title,title});
+});
+
+//about-us
+app.get('/about-us', (req, res) => {
+  var title = 'Our About Us Page';
+  res.render('pages/about-us',{title,title});
+});
+
+app.get('/texas-flooding', (req, res) => {
+  var title = 'Texas Flooding';
+  res.render('pages/texas-flooding',{title,title});
+});
+
+app.get('/little-wing', (req, res) => {
+  var title = 'Little Wing';
+  res.render('pages/little-wing',{title,title});
+});
+
+app.get('/little-pan-alley', (req, res) => {
+  var title = 'Little Pan Alley';
+  res.render('pages/little-pan-alley',{title,title});
 });
 
 app.listen(port, () => {
