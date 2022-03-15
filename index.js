120503f53bf64f7213e622fcc21da859f24518da
Evan J. Kang <95evankang@gmail.com>
2022-03-15 16:55:40 +0000

    users

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const port = 3000;
 
 app.set('view engine','ejs');
 
+var data = require('./data/test.json');
+
 //this will allow us to serve up static files, CSS, images & JS
 app.use(express.static(__dirname));
 
@@ -34,6 +36,28 @@ app.get('/little-pan-alley', (req, res) => {
   res.render('pages/little-pan-alley',{title,title});
 });
 
+//add users route
+app.get('/users', function(req, res) {
+	var title = 'Users Page';
+	res.render('users/index', {
+    	title: title,
+    	users: data
+	});
+});
+
+//add user/view route - we are cheating by using the array index - 1
+app.get('/users/view/:id', function(req, res) {
+ var title = 'User Page';
+ var id = req.params.id;
+ res.render('users/view', {
+     title: title,
+     user: data[--id]
+ });
+});
+
+
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
+  console.log(data);
 });
